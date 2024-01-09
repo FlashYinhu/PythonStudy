5135d91c31389ea74c8efb674dd62f4522611b5c
yinhu <kangyinhu@gmail.com>
2024-01-09 18:30:42 +0800

    2024/1/8 mac

---
+++ 10-2函数的嵌套调用.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+# Author : YinhuKang
+# E-mail : kangyinhunnu@gmail.com
+# Date : 2024/1/9 18:20
+# 在一个函数的里面调用另一个函数
+
+def f1():
+    print('11111111')
+
+
+def f2():
+    f1()
+    print('22222222')
+    print('33333333')
+
+
+# 调用
+f2()
