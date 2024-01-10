f3e8ff2d431ba9d2802e59c974e1135fd2f7760e
yinhu <kangyinhu@gmail.com>
2024-01-10 22:12:34 +0800

    2024/1/10 mac

--- 数据容器/01-3列表的遍历.py
+++ 数据容器/01-3列表的遍历.py
@@ -4,4 +4,20 @@
 # Date : 2024/1/10 17:24
 
 info = ['kangyinhu', 24, 187.56, True, [1, 2, 3]]
+print(len(info))
 
+# 遍历列表 if 不可以自定义循环条件
+# 理论上不可以无限循环，因为被遍历的内容不是无限的
+# 适用于遍历数据容器或者简单的固定次数循环的场景
+for i in range(len(info)):
+    print(info[i])
+print()
+
+# 遍历列表 while 可以自定义循环条件
+# 可以通过控制条件实现无限循环
+# 适用于任何想要实现循环的场景
+length = len(info)
+index = 0
+while index < length:
+    print(info[index])
+    index += 1
---
+++ 数据容器/02元组.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+# Author : YinhuKang
+# E-mail : kangyinhunnu@gmail.com
+# Date : 2024/1/10 21:00
+"""
+    元组tuple一旦定义完成，就不可进行修改（只读的列表）
+"""
+
+# 定义一个元组
+t1 = (1, "Hello", True)
+t2 = ()
+t3 = tuple()
+print(f't1的内容是{t1}, 类型是{type(t1)}')
+print(f't2的内容是{t2}, 类型是{type(t2)}')
+print(f't3的内容是{t3}, 类型是{type(t3)}')
+# 定义单个元素的元组
+t4 = ('kangyinhu')
+print(f't4的内容是{t4}, 类型是{type(t4)}')
+t5 = ('kangyinhu', )
+print(f't5的内容是{t5}, 类型是{type(t5)}')
+t6 = ('kangyinhu', ('xiaolei', 'zzz'))
+print(f't6的内容是{t6}, 类型是{type(t6)}')
+
