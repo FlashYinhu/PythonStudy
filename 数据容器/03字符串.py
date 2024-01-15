17ddbd5c73fa974daadda103b55700020ed3265f
Yinhu <kangyinhunnu@gmail.com>
2024-01-15 14:59:17 +0800

    2024/1/15

--- 数据容器/03字符串.py
+++ 数据容器/03字符串.py
@@ -7,3 +7,36 @@
     字符串是字符的容器 一个字符串可以存放任意数量的字符
 """
 
+name = 'kangyinhu'
+print(name)
+# 通过下标索引取值
+print(f'字符串中第五个字符是: {name[4]}')
+print(f'字符串中倒数第五个字符是: {name[-5]}')
+
+# 字符串同样是一个无法修改的数据容器
+# name[1] = 'z'
+# TypeError: 'str' object does not support item assignment
+# del name[0]
+# TypeError: 'str' object doesn't support item deletion
+# name.remove()
+# AttributeError: 'str' object has no attribute 'remove'
+# name.pop('a')
+# AttributeError: 'str' object has no attribute 'pop'
+# name.append('zhangrui')
+# AttributeError: 'str' object has no attribute 'append'
+
+# index 方法
+value = name.index('yin')
+print(f'在字符串中查找\'yin\', 其起始下标是{value}')
+
+# replace 方法
+
+
+print(len(name))
+for i in range(len(name)):
+    print(name[- i - 1], end='')
+    i += 1
+
+block = ['', '', '']
+
+
