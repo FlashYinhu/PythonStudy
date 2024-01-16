bfa72119aa619291b6d17a1ca91564314e71583c
Yinhu <kangyinhunnu@gmail.com>
2024-01-16 15:58:56 +0800

    2024/1/16

--- 数据容器/03字符串.py
+++ 数据容器/03字符串.py
@@ -30,7 +30,19 @@ value = name.index('yin')
 print(f'在字符串中查找\'yin\', 其起始下标是{value}')
 
 # replace 方法
+# 语法 字符串.replace(字符串1, 字符串2)
+# 将字符串内的全部字符串1 替换为字符串2
+# 注意并不是修改字符串本身 而是得到一个新的字符串
+print('将字符串中的\'yin\'替换为YIN')
+new_name = name.replace('yin', 'YIN')
+print(f'原来的字符串是: {name}\n替换后的字符串为:{new_name}\n替换后的字符串是新的字符串\n')
 
+# 字符串的分割
+# 语法： 字符串.split(分割字符串)
+# 按照指定的分隔符字符串将字符串划分为多个字符串并存入列表对象中
+str1 = 'abcdeAfghijkAlmnopqArstuvwAxyz'
+list1 = str1.split('A')
+print(f'原来的字符串为{str1}, 按照\'A\'分割后得到的新的对象为{list1}')
 
 print(len(name))
 for i in range(len(name)):
@@ -38,5 +50,4 @@ for i in range(len(name)):
     i += 1
 
 block = ['', '', '']
-
 
